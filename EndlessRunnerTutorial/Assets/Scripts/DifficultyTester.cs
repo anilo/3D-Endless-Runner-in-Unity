b46f8ef4366889b271fae07ffb3a628b6efcb422
mrzepisko <rz.marcin@gmail.com>
2021-03-02 22:05:47 +0000

    difficulty curve

---
+++ EndlessRunnerTutorial/Assets/Scripts/DifficultyTester.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class DifficultyTester : MonoBehaviour
+{
+    [Header("Current")]
+    [SerializeField] private float m_CurrentSpeed;
+    [SerializeField] private float m_CurrentDifficulty;
+
+    [Header("Manual")]
+    [SerializeField] private float m_DebugTime;
+    [SerializeField] private float m_DebugSpeed;
+    [SerializeField] private float m_DebugDifficulty;
+
+    private GameplayDifficulty m_Target;
+
+#if UNITY_EDITOR
+
+    private void Awake()
+    {
+        m_Target = FindObjectOfType<GameplayDifficulty>(); //just for debug
+    }
+
+    private void Update()
+    {
+        if (m_Target)
+        {
+            m_CurrentDifficulty = m_Target.DifficultyMultiplier;
+            m_CurrentSpeed = m_Target.SpeedMultiplier;
+
+            m_DebugDifficulty = m_Target.EvaluateDifficulty(m_DebugTime);
+            m_DebugSpeed = m_Target.EvaluateSpeed(m_DebugTime);
+        }
+    }
+#endif
+}
---
+++ EndlessRunnerTutorial/Assets/Scripts/GameplayDifficulty.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class GameplayDifficulty : MonoBehaviour, IDifficultyCurve
+{
+    public float DifficultyMultiplier => EvaluateDifficulty(Time.time);
+    public float SpeedMultiplier => EvaluateSpeed(Time.time);
+
+    [Header("Player movement")]
+    [SerializeField] private float m_BaseSpeed;
+    [SerializeField] private float m_MaxSpeed;
+    [SerializeField] private AnimationCurve m_SpeedCurve;
+    [SerializeField] private float m_TimeToMaxSpeed;
+
+    [Header("Obstacles")]
+    [SerializeField] private float m_DifficultyTick;
+    [Tooltip("Speed of difficulty raising")]
+    [SerializeField] private float m_RaiseFactor;
+    [Tooltip("Lower values will create more drastic differences between spikes")]
+    [SerializeField][Min(1)] private float m_SpikeAmplitudeFactor;
+    [Tooltip("Higher values will result in more frequent difficulty spikes and no spikes for 0")]
+    [SerializeField][Min(0)] private float m_SpikeFrequency;
+
+    public float EvaluateSpeed(float time)
+    {
+        float t = Mathf.Clamp01(time / m_TimeToMaxSpeed);
+        float curve = m_SpeedCurve.Evaluate(t);
+        float tt = Mathf.Clamp01(curve);
+        return Mathf.Lerp(m_BaseSpeed, m_MaxSpeed, tt);
+    }
+
+    /// difficulty curve based on this shape https://www.desmos.com/calculator/hnpcmxma0o
+    public float EvaluateDifficulty(float time)
+    {
+        float min = 1.0f;
+        float t = time / m_DifficultyTick;
+        float tt = t * t;
+        float a = m_SpikeFrequency, b = m_RaiseFactor, c = m_SpikeAmplitudeFactor;
+        
+        float S = Mathf.Sin(tt * a);
+        float B = tt * b;
+        float A = S / (c * t);
+        float d = min + A + B;
+
+        return Mathf.Clamp(d, min, Mathf.Abs(d));
+    }
+}
