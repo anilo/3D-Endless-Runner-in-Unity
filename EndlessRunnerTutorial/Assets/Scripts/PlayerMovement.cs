a7b7fb30ed606c20e5703dfdb4a8450c33f86cde
BhavsarJay <bhavsarjay01@gmail.com>
2021-03-03 00:19:57 +0530

    Add Jump and opposite lane switch to player movement
    1. Player can Jump
    2. Player can switch to the direct opposite lane

--- EndlessRunnerTutorial/Assets/Scripts/PlayerMovement.cs
+++ EndlessRunnerTutorial/Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,16 +9,23 @@ public class PlayerMovement : MonoBehaviour {
     public float speed = 5;
     [SerializeField] float horizontalMultiplier = 2;
     public float speedIncreasePerPoint = 0.1f;
+    public AnimationCurve jumpTrajectory;
 
     private float horizontalInput;
     private bool m_alive = true;
     private Vector2 m_currentLane;
+    private int m_currentLaneIndex;
     private Vector3 m_currentVel;
     public float rotSpeed = 5f;
+    private float timePeriod;
+    private float timeElapsed;
+    public bool isJumping;
+    private float jumpTime = 1.5f;
 
     private void Start()
     {
-        m_currentLane = new Vector2(2.5f, 0);
+        m_currentLane = new Vector2(-2, 0);
+        m_currentLaneIndex = 0;
     }
 
 
@@ -27,7 +35,7 @@ public class PlayerMovement : MonoBehaviour {
         if (!m_alive) return;
 
         // Forward Movement
-        Vector3 forwardMove = Vector3.forward * speed * Time.fixedDeltaTime;
+        Vector3 forwardMove = Vector3.forward * speed * Time.deltaTime;
         transform.position = transform.position + forwardMove;
 
         if (Input.GetKeyDown(KeyCode.A))
@@ -36,36 +44,99 @@ public class PlayerMovement : MonoBehaviour {
             SwitchLanes(1);
 
         Vector3 targetpos = new Vector3(m_currentLane.x, m_currentLane.y, transform.position.z);
-        Vector3 smoothedPos = Vector3.SmoothDamp(transform.position, targetpos, ref m_currentVel, 0.1f);
-        transform.position = smoothedPos;
 
         //Rotate Player based on his current lane
         RotatePlayer();
+
+        // Jump
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            isJumping = true;
+            timeElapsed = 0f;
+            timePeriod = 1f;
+        }
+        if (isJumping)
+        {
+            if (timeElapsed < timePeriod)
+            {
+                float t = (timeElapsed / timePeriod);
+                float y = jumpTrajectory.Evaluate(t);
+                //Change the targetpos here for jumping
+                targetpos = targetpos + transform.up * y;
+
+                timeElapsed += Time.deltaTime * jumpTime;
+            }
+            else
+                isJumping = false;
+
+            if (timeElapsed > 0.1f && timeElapsed < 0.3f && Input.GetKeyDown(KeyCode.Space))
+            {
+                timeElapsed = timePeriod - 0.1f;
+                SwitchLanes(69);
+            }
+        }
+
+        Vector3 smoothedPos = Vector3.SmoothDamp(transform.position, targetpos, ref m_currentVel, 0.1f);
+        transform.position = smoothedPos;
     }
 
+
+    Vector2[] Lanes =
+    {
+        new Vector2(-2, 0), 
+        new Vector2(2, 0),  
+        new Vector2(6, 4),  
+        new Vector2(6, 8),  
+        new Vector2(2, 12), 
+        new Vector2(-2, 12),
+        new Vector2(-6, 8), 
+        new Vector2(-6, 4)  
+    };
+
+    //        -2,12    2,12
+    // -6,8                   6,8
+    // -6,4                   6,4
+    //        -2,0     2,0
+
+    // Opposite Lanes
+    // 0 - 5
+    // 1 - 4
+    // 2 - 7
+    // 3 - 6
+    // 4 - 1
+    // 5 - 0
+    // 6 - 3
+    // 7 - 2            
     private void SwitchLanes(int input)
     {
+        var nextIndex = m_currentLaneIndex + 1;
+        var prevIndex = m_currentLaneIndex - 1;
+        var oppIndex = m_currentLaneIndex;
+
+        if (m_currentLaneIndex == 7)
+            nextIndex = 0;
+        else if(m_currentLaneIndex == 0)
+            prevIndex = 7;
+
         if (input == 1f)
         {
-            if (m_currentLane.x == -2.5f && m_currentLane.y == 0) m_currentLane = new Vector2(2.5f, 0);
-            else if (m_currentLane.x == 2.5f && m_currentLane.y == 0) m_currentLane = new Vector2(5f, 2.5f);
-            else if (m_currentLane.x == 5f && m_currentLane.y == 2.5f) m_currentLane = new Vector2(5f, 7.5f);
-            else if (m_currentLane.x == 5f && m_currentLane.y == 7.5f) m_currentLane = new Vector2(2.5f, 10f);
-            else if (m_currentLane.x == 2.5f && m_currentLane.y == 10f) m_currentLane = new Vector2(-2.5f, 10f);
-            else if (m_currentLane.x == -2.5f && m_currentLane.y == 10f) m_currentLane = new Vector2(-5f, 7.5f);
-            else if (m_currentLane.x == -5f && m_currentLane.y == 7.5f) m_currentLane = new Vector2(-5f, 2.5f);
-            else if (m_currentLane.x == -5f && m_currentLane.y == 2.5f) m_currentLane = new Vector2(-2.5f, 0f);
+            m_currentLane = Lanes[nextIndex];
+            m_currentLaneIndex = nextIndex;
         }
         else if (input == -1f)
         {
-            if (m_currentLane.x == -2.5f && m_currentLane.y == 0) m_currentLane = new Vector2(-5f, 2.5f);
-            else if (m_currentLane.x == 2.5f && m_currentLane.y == 0) m_currentLane = new Vector2(-2.5f, 0f);
-            else if (m_currentLane.x == 5f && m_currentLane.y == 2.5f) m_currentLane = new Vector2(2.5f, 0f);
-            else if (m_currentLane.x == 5f && m_currentLane.y == 7.5f) m_currentLane = new Vector2(5f, 2.5f);
-            else if (m_currentLane.x == 2.5f && m_currentLane.y == 10f) m_currentLane = new Vector2(5f, 7.5f);
-            else if (m_currentLane.x == -2.5f && m_currentLane.y == 10f) m_currentLane = new Vector2(2.5f, 10f);
-            else if (m_currentLane.x == -5f && m_currentLane.y == 7.5f) m_currentLane = new Vector2(-2.5f, 10f);
-            else if (m_currentLane.x == -5f && m_currentLane.y == 2.5f) m_currentLane = new Vector2(-5f, 7.5f);
+            m_currentLane = Lanes[prevIndex];
+            m_currentLaneIndex = prevIndex;
+        }
+        else if(input == 69)
+        {
+            Debug.Log("Jump To Opposite Lane");
+            if (m_currentLaneIndex == 1 || m_currentLaneIndex == 3) m_currentLaneIndex += 3;
+            else if (m_currentLaneIndex == 0 || m_currentLaneIndex == 2) m_currentLaneIndex += 5;
+            else if (m_currentLaneIndex == 4 || m_currentLaneIndex == 6) m_currentLaneIndex -= 3;
+            else if (m_currentLaneIndex == 5 || m_currentLaneIndex == 7) m_currentLaneIndex -= 5;
+
+            m_currentLane = Lanes[m_currentLaneIndex];
         }
     }
 
@@ -74,9 +145,9 @@ public class PlayerMovement : MonoBehaviour {
         float rotAngle = 0f;
 
         if (m_currentLane.y == 0f) rotAngle = 0;
-        else if (m_currentLane.x == 5f) rotAngle = 90f;
-        else if (m_currentLane.y == 10f) rotAngle = -180f;
-        else if (m_currentLane.x == -5f) rotAngle = -90f;
+        else if (m_currentLane.x == 6f) rotAngle = 90f;
+        else if (m_currentLane.y == 12f) rotAngle = -180f;
+        else if (m_currentLane.x == -6f) rotAngle = -90f;
 
         Quaternion targetRot = Quaternion.Euler(0, 0, rotAngle);
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * rotSpeed);
