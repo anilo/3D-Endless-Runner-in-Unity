2e5b34530b1150125388016059491bb40e378711
Anil Punjabi <anilo@hotmail.com>
2021-03-03 15:30:22 -0800

    Scores working based on distance travelled

--- EndlessRunnerTutorial/Assets/Scripts/GameManager.cs
+++ EndlessRunnerTutorial/Assets/Scripts/GameManager.cs
@@ -9,17 +9,17 @@ public class GameManager : MonoBehaviour {
     int score;
     public static GameManager inst;
 
-    [SerializeField] Text scoreText;
+//anil commented    [SerializeField] Text scoreText;
 
     [SerializeField] PlayerMovement playerMovement;
 
-    public void IncrementScore ()
-    {
-        score++;
-        scoreText.text = "SCORE: " + score;
-        // Increase the player's speed
-        playerMovement.speed += playerMovement.speedIncreasePerPoint;
-    }
+    // anil commented public void IncrementScore ()
+    // {
+    //     score++;
+    //     scoreText.text = "SCORE: " + score;
+    //     // Increase the player's speed
+    //     playerMovement.speed += playerMovement.speedIncreasePerPoint;
+    // }
 
     private void Awake ()
     {
